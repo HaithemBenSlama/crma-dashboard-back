5bcd2b1f6faead12f86539c1c23f55c63e68db67
Haithem Ben Slama <Haithem.benslama@hotmail.fr>
2025-03-08 03:47:48 +0100

    Initialisation du projet CRMA Dashboard Backend avec Spring Boot Ajout des dépendances requises : Spring Boot, PostgreSQL, JWT, Spring Security et outils associés

---
+++ src/main/java/com/crma/CrmaDashboardBackApplication.java
@@ -0,0 +1,13 @@
+package com.crma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmaDashboardBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmaDashboardBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crma/CrmaDashboardBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmaDashboardBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
